9201b08cb7dacf9f7a8e297ebd979db3787c581b
Daniel Agg <daniel.agg@outlook.com>
2023-09-01 20:30:41 +0200

    site structure

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        main: {
+          red: "#e61c29",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
